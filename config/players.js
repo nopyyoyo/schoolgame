5f4ab8094826faef750072052c011fbd034c932c
nopyyoyo <nopy_yoyo@hotmail.com>
2026-09-15 15:09:02 -0500

    Add files via upload

--- config/players.js
+++ config/players.js
@@ -9,9 +9,9 @@ window.PORTAL_PLAYERS = [
     ownedItems: ["item_01", "item_04"] },
   { id: "p2", team: "red", name: "มัลคอม", face: "P2S_Cut", hpMax: 34, mpMax: 13, attack: 15, defense: 8, speed: 6, wisdom: 1, money: 50 },
   { id: "p3", team: "red", name: "ตริณ", face: "P3S_Cut", hpMax: 25, mpMax: 22, attack: 8, defense: 4, speed: 7, wisdom: 1, money: 50 },
-  { id: "p4", team: "red", name: "อวาลิน", face: "P4S_Cut", hpMax: 50, mpMax: 11, attack: 6, defense: 10, speed: 5, wisdom: 1, money: 50 },
+  { id: "p4", team: "blue", name: "อวาลิน", face: "P4S_Cut", hpMax: 50, mpMax: 11, attack: 6, defense: 10, speed: 5, wisdom: 1, money: 50 },
   { id: "p5", team: "blue", name: "เจสซี่", face: "P5S_Cut", hpMax: 30, mpMax: 10, attack: 10, defense: 7, speed: 10, wisdom: 1, money: 50 },
   { id: "p6", team: "blue", name: "ไนล์ส", face: "P6S_Cut", hpMax: 30, mpMax: 10, attack: 10, defense: 7, speed: 10, wisdom: 1, money: 50 },
-  { id: "p7", team: "blue", name: "โซเฟีย", face: "P7S_Cut", hpMax: 30, mpMax: 10, attack: 10, defense: 7, speed: 10, wisdom: 1, money: 50 },
-  { id: "p8", team: "blue", name: "ซูริ", face: "P8S_Cut", hpMax: 30, mpMax: 10, attack: 10, defense: 7, speed: 10, wisdom: 1, money: 50 }
+  { id: "p7", team: "green", name: "โซเฟีย", face: "P7S_Cut", hpMax: 30, mpMax: 10, attack: 10, defense: 7, speed: 10, wisdom: 1, money: 50 },
+  { id: "p8", team: "green", name: "ซูริ", face: "P8S_Cut", hpMax: 30, mpMax: 10, attack: 10, defense: 7, speed: 10, wisdom: 1, money: 50 }
 ];
