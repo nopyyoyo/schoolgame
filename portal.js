80bfcda6c988e0079c6fa82fb5de8f07d3721bc9
nopyyoyo <nopy_yoyo@hotmail.com>
2026-09-15 15:52:09 -0500

    Add files via upload

--- portal.js
+++ portal.js
@@ -8,7 +8,8 @@
     : null;
   const root = document.querySelector("#player-view");
   const shopRoot = document.querySelector("#shop-view");
-  const image = (player) => `Character/Cut/Player/${player.face}/face.png`;
+  const siteRoot = new URL("./", document.baseURI).href;
+  const image = (player) => `${siteRoot}Character/Cut/Player/${player.face}/face.png`;
   const params = new URLSearchParams(window.location.search);
   const selected = players.find((player) => player.id === params.get("player"));
   const categoryNames = { weapon: "อาวุธ", armor: "เกราะ", shield: "โล่", accessory: "เครื่องประดับ", item: "ไอเทม" };
