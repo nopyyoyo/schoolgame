67c84c14e45c8f8747afcdaafc55e392e4eab108
nopyyoyo <nopy_yoyo@hotmail.com>
2026-09-17 19:48:07 -0500

    Add files via upload

--- app/music.js
+++ app/music.js
@@ -1,6 +1,6 @@
 window.MUSIC_CONFIG = {
-  battle: "../Music/105%20Battle%20Theme.mp3",
-  victory: "../Music/106%20Fanfare.mp3",
+  battle: "Music/105%20Battle%20Theme.mp3",
+  victory: "Music/106%20Fanfare.mp3",
   battleVolume: 0.45,
   victoryVolume: 0.7
 };
--- app/sounds.js
+++ app/sounds.js
@@ -1,8 +1,8 @@
 window.SOUND_CONFIG = {
-  hit: "../Sounds/33HitFist.wav",
-  miss: "../Sounds/0DMiss.wav",
-  enemyDeath: "../Sounds/2DMonsterDeath.wav",
-  playerTurn: "../Sounds/DCMenuTing.wav",
+  hit: "Sounds/33HitFist.wav",
+  miss: "Sounds/0DMiss.wav",
+  enemyDeath: "Sounds/2DMonsterDeath.wav",
+  playerTurn: "Sounds/DCMenuTing.wav",
   volumes: {
     hit: 0.8,
     miss: 0.8,
