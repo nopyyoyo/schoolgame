95b6078995be51d4a75d7aa3d99dc70adaf45b61
nopyyoyo <nopy_yoyo@hotmail.com>
2026-09-17 19:00:59 -0500

    Add files via upload

---
+++ config/characters.js
@@ -0,0 +1,14 @@
+window.CHARACTER_CONFIG = {
+  players: [
+    { id: "p1", name: "เอมิลี่", face: "P1S_Cut", hpMax: 30, mpMax: 10, attack: 10, defense: 7, speed: 10, wisdom: 1 },
+    { id: "p2", name: "มัลคอม", face: "P2S_Cut", hpMax: 34, mpMax: 13, attack: 15, defense: 8, speed: 6, wisdom: 1 },
+    { id: "p3", name: "ตริณ", face: "P3S_Cut", hpMax: 25, mpMax: 22, attack: 8, defense: 4, speed: 7, wisdom: 1 },
+    { id: "p4", name: "อวาลิน", face: "P4S_Cut", hpMax: 50, mpMax: 11, attack: 6, defense: 10, speed: 5, wisdom: 1 }
+  ],
+  enemies: [
+    { id: "e1", name: "ทหาร 1", image: "enemy_01.png", hpMax: 30, mpMax: 0, attack: 10, defense: 5, speed: 7, wisdom: 1 },
+    { id: "e2", name: "ทหาร 2", image: "enemy_02.png", hpMax: 30, mpMax: 0, attack: 10, defense: 5, speed: 7, wisdom: 1 },
+    { id: "e3", name: "ทหาร 3", image: "enemy_03.png", hpMax: 30, mpMax: 0, attack: 10, defense: 5, speed: 7, wisdom: 1 },
+    { id: "e4", name: "หัวหน้าทหาร", image: "enemy_04.png", hpMax: 100, mpMax: 0, attack: 13, defense: 8, speed: 9, wisdom: 1 }
+  ]
+};
