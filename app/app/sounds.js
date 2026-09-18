ed9b56102f2be24db892936417e521463cb8fa91
nopyyoyo <nopy_yoyo@hotmail.com>
2026-09-17 19:32:50 -0500

    Add files via upload

---
+++ app/app/sounds.js
@@ -0,0 +1,12 @@
+window.SOUND_CONFIG = {
+  hit: "../Sounds/33HitFist.wav",
+  miss: "../Sounds/0DMiss.wav",
+  enemyDeath: "../Sounds/2DMonsterDeath.wav",
+  playerTurn: "../Sounds/DCMenuTing.wav",
+  volumes: {
+    hit: 0.8,
+    miss: 0.8,
+    enemyDeath: 0.9,
+    playerTurn: 0.8
+  }
+};
