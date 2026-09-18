a3b3365a4ea4212fc83834a292ed28a74f97adbb
nopyyoyo <nopy_yoyo@hotmail.com>
2026-09-18 15:59:44 -0500

    Update enemyIds for battle level 1

--- config/battle-levels.js
+++ config/battle-levels.js
@@ -1,5 +1,5 @@
 window.BATTLE_LEVELS = [
-  { id: 1, name: "ลานประลองขั้นที่ 1", enemyIds: ["e1","e1", "e2"], open: true },
+  { id: 1, name: "ลานประลองขั้นที่ 1", enemyIds: ["e1","e2", "e3"], open: true },
   { id: 2, name: "ลานประลองขั้นที่ 2", enemyIds: [], open: false },
   { id: 3, name: "ลานประลองขั้นที่ 3", enemyIds: [], open: false }
 ];
