c03f9213f59749f98eac827f2dd424a5bb052ca0
nopyyoyo <nopy_yoyo@hotmail.com>
2026-09-17 19:44:23 -0500

    Delete app/app directory

--- app/app/battle-roster.js
+++
@@ -1,4 +0,0 @@
-window.BATTLE_ROSTER_CONFIG = {
-  playerIds: ["p1", "p2", "p3", "p4"],
-  enemyIds: ["e1", "e2"]
-};
--- app/app/music.js
+++
@@ -1,6 +0,0 @@
-window.MUSIC_CONFIG = {
-  battle: "../Music/105%20Battle%20Theme.mp3",
-  victory: "../Music/106%20Fanfare.mp3",
-  battleVolume: 0.45,
-  victoryVolume: 0.7
-};
